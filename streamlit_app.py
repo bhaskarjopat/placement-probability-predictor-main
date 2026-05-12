ad83d5e3462e49d2150b34b1e0ea789b1cbf95d3
bhaskarjopat <bhaskarjopat005@gmail.com>
2026-05-12 12:09:52 +0530

    Refactor probability computation to use backend API

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,5 @@
 import streamlit as st
-import requests
-import config
-
-def compute_probability(payload):
-    r = requests.post(url="http://127.0.0.1:8000/compute_probability", json=payload)
-    return r.json()
+from backend import determine_placement_posterior_probability
 
 st.set_page_config(page_title="Placement Probability Predictor", layout="centered")
 st.title("Placement Probability Predictor")
@@ -17,10 +12,8 @@ cgpa = st.number_input(label="Enter your CGPA here", min_value=0.0, max_value=10
 communication_skills = st.number_input("Enter your communication skills rating here", min_value=0, max_value=10, step=1)
 projects_completed = st.number_input("Enter number of projects completed by you", min_value=0, max_value=5, step=1)
 
-payload = {"iq":iq , "previous_semester_result":previous_semester_result, "cgpa":cgpa, "communication_skills":communication_skills, "projects_completed":projects_completed }
-
-text_area_placeholder =st.empty()
+text_area_placeholder = st.empty()
 if st.button("Calculate Probability of Getting Placed"):
-    response = compute_probability(payload)
+    values = [iq, previous_semester_result, cgpa, communication_skills, projects_completed]
+    response = determine_placement_posterior_probability(values)
     text_area_placeholder.text_area(label="Your Placement Probability Result", value=response["result"], height=200)
-    
